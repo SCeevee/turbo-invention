31a04437a52bb535d54fc10a1d6c26d4392a5b4a
SCeevee <77897036+SCeevee@users.noreply.github.com>
2022-08-22 20:50:05 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+noseX = 0;
+noseY = 0;
+
+function preload() {
+	world_start = loadSound("world_start.wav");
+	mario_jump = loadSound("jump.wav")
+	mario_coin = loadSound("coin.wav")
+	mario_gameover = loadSound("gameover.wav")
+	mario_kill = loadSound("kick.wav")
+	mario_die = loadSound("mariodie.wav")
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	instializeInSetup(mario);
+	canvas.parent("canvas");
+	video = createCapture(VIDEO);
+    video.size(800,400);
+	video.parent("gameConsole");
+	poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw() {
+	game()
+}
+
+
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+    console.log(results);
+	noseX = results[0].pose.nose.x;
+    noseY = results[0].pose.nose.y;
+    
+  }
+}
+
+  function modelLoaded() {
+	console.log('model_Loaded/-<{');
+  }
+  
+
+
